5c1a1301f5097fc79d51c4c936def14fc95b632f
MatSaf123 <xnerov13@gmail.com>
2021-05-02 19:48:24 +0200

    Add registering, logging in, logging out

--- pet_care_app/settings.py
+++ pet_care_app/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
+
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'posts-home'
+
+LOGIN_URL = 'login'
---
+++ users/admin.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.contrib import admin
+
+
+# Register your models here.
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
