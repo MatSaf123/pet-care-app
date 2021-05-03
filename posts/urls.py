8ce27c480108e16137396cc5f375f318ad8b9f03
MatSaf123 <xnerov13@gmail.com>
2021-05-03 18:14:11 +0200

    Delete post from detail view

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from .views import detail_view, PostCreateView, tagged
+from .views import detail_view, tagged, PostCreateView, PostDeleteView
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about, name='about'),
+
     path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<slug:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
     path('post/<slug:slug>/', detail_view, name="post-detail"),
     path('tag/<slug:slug>/', tagged, name="tagged"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, get_object_or_404
 from django.template.defaultfilters import slugify
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from .models import Post
 from taggit.models import Tag
@@ -48,15 +48,31 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content', 'tags']
 
+    post_id = str(1 + Post.objects.latest('id').id)
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         new_post = form.save(commit=False)
-        new_post.slug = slugify(new_post.title)
+
+        # in order to create unique slug, id number is added at the end of it
+        new_post.slug = slugify(''.join([new_post.title, self.post_id]))
 
         new_post.save()
         form.save_m2m()
 
         return super().form_valid(form)
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    # check if active user is the original poster
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
