04afedd3d90d9363f70a8d43e35334008b498f8e
MatSaf123 <xnerov13@gmail.com>
2021-05-03 15:55:41 +0200

    Add tags to posts, temporary crispy form in post_form because of POST bug

--- posts/models.py
+++ posts/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
+
 
 class Post(models.Model):
     """Post model: a help request or help offer posted by a registered user"""
@@ -13,10 +15,12 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, max_length=100, default=None)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         """Redirect user to home page after a post is created"""
-        return reverse('posts-home')
+        return reverse('home')
